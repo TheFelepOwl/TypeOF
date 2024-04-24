5aab08b0196a4d9e84f1d77d0247400db010c1d2
The Felep Owl <denisgavr04@gmail.com>
2024-04-24 21:24:54 +0200

    Add project files.

---
+++ Typeof/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Coordinates
+{
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
+
+public enum SensorSignal
+{
+    moisture,
+    temperature,
+    light
+}
+
+public class SensorData
+{
+    public SensorSignal Signal_type { get; set; }
+    public double Value { get; set; }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // Приклад використання Coordinates
+        Coordinates place = new Coordinates { Latitude = 40.7128, Longitude = -74.0060 };
+        Console.WriteLine($"Координати: {place.Latitude}, {place.Longitude}");
+
+        
+
+        // Приклад використання SensorData
+        SensorData date_temperature = new SensorData { Signal_type = SensorSignal.temperature, Value = 25.5 };
+        Console.WriteLine($"Данi датчика: Тип сигналу - {date_temperature.Signal_type}, Значення - {date_temperature.Value}`C");
+
+        // Приклад використання SensorData
+        SensorData date_moisture = new SensorData { Signal_type = SensorSignal.moisture, Value = 80 };
+        Console.WriteLine($"Данi датчика: Тип сигналу - {date_moisture.Signal_type}, Значення - {date_moisture.Value}%");
+    }
+}
